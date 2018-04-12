9d6b3b827c98b993b7da89f05f4c9e84a3749764
David Brady <dgrbrady@gmail.com>
2018-04-12 11:19:25 -0400

    Finished front end work for navbar WITH MULTI LEVEL MENUS WOOOOOO. Also added Logout function to login button and fixed bug where front end wouldn't display logged in unless you logged in twice

--- app/main/views.py
+++ app/main/views.py
@@ -1,7 +1,26 @@
 from datetime import datetime
 from flask import render_template, session, redirect, url_for
 from . import main
+from .forms import LoginForm
 
 @main.route('/', methods=['GET', 'POST'])
 def index():
-    return render_template("index.html")
+    form = LoginForm()
+    current_name = session.get('name')
+    if current_name is not None:
+        session['logged_in'] = True
+
+    if form.validate_on_submit():
+        session['name'] = form.name.data
+        session['password'] = form.password.data
+        return redirect(url_for('main.index'))
+    return render_template("index.html", 
+                            form=form, 
+                            name=session.get('name'),
+                            logged_in=session.get('logged_in'))
+
+@main.route('/logout', methods=['GET', 'POST'])
+def logout():
+    session['logged_in'] = False
+    session['name'] = None
+    return redirect(url_for('main.index'))
