6b2136457c7a9e33e092855913e1d31aff7e01f3
David Brady <dgrbrady@gmail.com>
2018-04-13 11:49:44 -0400

    Added SQLite to project

---
+++ app/models.py
@@ -0,0 +1,18 @@
+from app import db
+class User(db.Model):
+    __tablename__ = 'users'
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(64), index=True, unique=True)
+    password_hash = db.Column(db.String(128))
+    assigned_role = db.Column(db.Integer, db.ForeignKey('role.id'))
+
+    def __repr__(self):
+        return '<User: {}>'.format(self.username)
+
+class Role(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(64), unique=True)
+    users = db.relationship('User', backref='role')
+
+    def __repr__(self):
+        return '<Role: {}'.format(self.name)
