3bfd620f7d0cf6641c3ed027b743949c0da0a59a
David Brady <dgrbrady@gmail.com>
2018-04-13 13:03:53 -0400

    Added set_password() and check_password() to User model

--- app/models.py
+++ app/models.py
@@ -1,4 +1,6 @@
 from app import db
+from werkzeug.security import generate_password_hash, check_password_hash
+
 class User(db.Model):
     __tablename__ = 'users'
     id = db.Column(db.Integer, primary_key=True)
@@ -6,6 +8,12 @@ class User(db.Model):
     password_hash = db.Column(db.String(128))
     assigned_role = db.Column(db.Integer, db.ForeignKey('role.id'))
 
+    def set_password(self, password):
+        self.password_hash = generate_password_hash(password)
+    
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)
+        
     def __repr__(self):
         return '<User: {}>'.format(self.username)
 
