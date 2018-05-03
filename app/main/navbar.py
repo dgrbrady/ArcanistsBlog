157db08b7b186378280d56770ef4e7b228e561ae
David Brady <dgrbrady@gmail.com>
2018-05-03 13:57:53 -0400

    Added reST docstrings for navbar.py

--- app/main/navbar.py
+++ app/main/navbar.py
@@ -1,5 +1,23 @@
 class NavBar(object):
-    # TODO: Documentation
+    """Objects from this class hold a dict of items to populate the navigation bar.
+
+    Class attributes:
+        'menus' - dict:
+            Each key represents what is going to be displayed on the
+            navigation bar and for the purposes of this documentation will be
+            referred to as 'menus'. Every menu requires a tuple containin
+            their sub-menus. If no sub-menu is required, the tuple must
+            explicitely contain the None value. If a sub-menu has a sub-menu,
+            it must be represented as a dict with the key being the value you
+            want displayed in the parent sub-menu and the value being a tuple
+            containing the child sub-menu.
+
+    Class methods:
+        is_empty():
+            Given a menu (key), the function will return true if the sub-menu
+            (value) is None.
+    """
+
     menus = {
         'IOCs': (
             'Indicators',
@@ -48,8 +66,15 @@ class NavBar(object):
         'Tools': (None)
     }
 
-    # TODO: Documentation
     def is_empty(self, menu):
+        """Pass a menu as a string to check if the menu contains any sub-menus.
+
+        :param self: instance of NavBar.
+        :param menu: *string* menu item to be evaluated.
+
+        :returns: *bool* True if menu is empty, else returns False.
+        """
+
         if self.menus[menu] is None:
             return True
         else:
